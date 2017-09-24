a094cd471391ad0e2147181d08bfefdf4270a4a3
Eric Yihan Chen <ericyihc@usc.edu>
2017-09-23 19:32:59 -0500

    command line menu recording, replaying, mixing

--- src/sample.cpp
+++ src/sample.cpp
@@ -9,12 +9,13 @@ using namespace Leap;
 
 const std::string stateNames[] = {"STATE_INVALID", "STATE_START", "STATE_UPDATE", "STATE_END"};
 const std::string beatNames[] = {"closed-hi-hat", "kickA", "kickB", "open-hi-hat", "snare"};
+const int swipe_max = 14;
+const int cir_max = 32;
+
 int beat_index = -1;
 int swipe_ctr = 0;
 int cir_ctr = 0;
-
-const int swipe_max = 14;
-const int cir_max = 32;
+//int response;
 
 void sound(int index) {
     
@@ -46,6 +47,8 @@ void SampleListener::onFrame(const Controller& controller) {
 
     beat_index = -1; // initialize it to unvalid value
     Frame frame = controller.frame();
+    //response = frame.fingers().count();
+
     // Get gestures
     const GestureList gestures = frame.gestures();
     for (int g = 0; g < gestures.count(); ++g) {
@@ -71,6 +74,7 @@ void SampleListener::onFrame(const Controller& controller) {
           clockwiseness = "counterclockwise";
         }
 
+        /*
         // Calculate angle swept since last frame
         float sweptAngle = 0;
         if (circle.state() != Gesture::STATE_START) {
@@ -84,6 +88,7 @@ void SampleListener::onFrame(const Controller& controller) {
                   << ", radius: " << circle.radius()
                   << ", angle " << sweptAngle * RAD_TO_DEG
                   <<  ", " << clockwiseness << std::endl;
+        */
         break;
       }
       case Gesture::TYPE_SWIPE:
@@ -95,34 +100,39 @@ void SampleListener::onFrame(const Controller& controller) {
         } else if (swipe_ctr > swipe_max) {
             swipe_ctr = 0;
         }
-   
+        /*
         SwipeGesture swipe = gesture;
         std::cout << std::string(2, ' ')
           << "Swipe id: " << gesture.id()
           << ", state: " << stateNames[gesture.state()]
           << ", direction: " << swipe.direction()
           << ", speed: " << swipe.speed() << std::endl;
+        */
         break;
       }
       case Gesture::TYPE_KEY_TAP:
       {
         beat_index = 0;
+        /*
         KeyTapGesture tap = gesture;
         std::cout << std::string(2, ' ')
           << "Key Tap id: " << gesture.id()
           << ", state: " << stateNames[gesture.state()]
           << ", position: " << tap.position()
           << ", direction: " << tap.direction()<< std::endl;
+        */
         break;
       }
       case Gesture::TYPE_SCREEN_TAP:
       {
+        /*
         ScreenTapGesture screentap = gesture;
         std::cout << std::string(2, ' ')
           << "Screen Tap id: " << gesture.id()
           << ", state: " << stateNames[gesture.state()]
           << ", position: " << screentap.position()
           << ", direction: " << screentap.direction()<< std::endl;
+        */
         break;
       }
       default:
@@ -137,16 +147,66 @@ void SampleListener::onFrame(const Controller& controller) {
 }
 
 int main (int argc, char** argv) {
+    int response = 0;
+    int file_ctr = 0;
+
     SampleListener lisenter;
     Controller controller;
 
-    controller.addListener(lisenter);
-    system("osascript QTRecord.scpt");
+    while (1) {
+        std::string file_pre = "beats";
+        std::string file_name = "";
+        std::string record_cmd = "sox -b 32 -e unsigned-integer -r 96k -c 2 -d --clobber --buffer $((96000*2*10)) ";
+        std::string play_cmd = "play ";
+        std::string mix_cmd = "sox --combine mix";
 
-    std::cout << "Press Enter to quit..." << std::endl;
-    std::cin.get();
+        std::cout << "menu" << std::endl;
+        std::cout << "options:" << std::endl;
+        std::cout << "1 for recording 10 seconds" << std::endl;
+        std::cout << "2 for replay latest beats" << std::endl;
+        std::cout << "3 for a magic merge" << std::endl;
+        std::cout << "4 for playing the merged beats" << std::endl;
+        std::cout << "-1 for exit" << std::endl; 
+        std::cin >> response;
 
-    controller.removeListener(lisenter);
+        std::cout << response << std::endl;
+        if (response == 1) {
+            file_ctr++;
+
+            file_name = file_pre + (char) (file_ctr + '0') + ".wav";
+            controller.addListener(lisenter);
+            record_cmd += file_name + " trim 0 10";
+
+            // record for 10 secs
+            system(record_cmd.c_str());
+            controller.removeListener(lisenter);
+
+        } else if (response == 2) {
+            file_name = file_pre + (char) (file_ctr + '0') + ".wav";
+            play_cmd += file_name; 
+            system(play_cmd.c_str());
+        } else if (response == 3) {
+            if (file_ctr > 1) {
+                for (int i = 1; i <= file_ctr; i++) {
+                    mix_cmd += " " + file_pre + (char)('0' + i) + ".wav";
+                }
+                mix_cmd += " mixed.wav";
+                std::cout << mix_cmd << std::endl;
+                system(mix_cmd.c_str());
+            }
+        } else if (response == 4) {
+            play_cmd += "mixed.wav";
+            system(play_cmd.c_str());
+        } else if (response == -1) {
+            break;
+        } else if (response == 0) {
+        } else {
+            std::cout << "invalide input" << std::endl; 
+            break;
+        } 
+    }
+
+    std::cout << "Thanks for using!" << std::endl;
 
     return 0;
 }
