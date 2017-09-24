3f948191dae4ca476fe456252501d8ebf242c27a
leoybkim <leoybkim@gmail.com>
2017-09-24 04:42:54 -0500

    Update python script to read and delete option signal from firebase

--- src/firebase-read.py
+++ src/firebase-read.py
@@ -7,9 +7,17 @@ FIREBASE_URL = "https://fingerbeats-a2bf3.firebaseio.com/"
 if __name__ == '__main__':
     fb = firebase.FirebaseApplication(FIREBASE_URL, None) # Create a reference to the Firebase Application
 
-    while 1:
-        result = fb.get('', "input") # Get data from firebase
-        print(result)
+    result = fb.get('', 'input') # Get data from firebase
+    if result is None:
         F = open("test.txt", 'w')
-        F.write(str(result))
+        F.write(str(-2))         # error signal for Cpp
         F.close()
+    else :
+        for k in result.keys():
+            F = open("test.txt", 'w')
+            F.write(str(result[k]['option']))
+            F.close()
+            try:
+                fb.delete(str('/input/'+k), 'option')
+            except:
+                print('Error occured while updating firebase')
